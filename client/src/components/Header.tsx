ae69b8f9da9dd66091e11f22e9e25d80db172745
Joseph Maramba <joseph@smartmile.eu>
2023-01-27 12:43:54 +0300

    setup components and pages

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { logo } from '../assets';
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 py-4 border-b border-b-[#e6ebf4]">
+      Yoooh
+    </header>
+  );
+};
+
+export default Header;
---
+++ client/src/pages/CreatePost.tsx
@@ -0,0 +1,10 @@
+
+type Props = {}
+
+const CreatePost = (props: Props) => {
+  return (
+    <div>CreatePost</div>
+  )
+}
+
+export default CreatePost
