5ba4673e1bf5ad6d046f03fb2a7ef3ae82bb8206
Joseph Maramba <joseph@smartmile.eu>
2023-01-27 12:28:23 +0300

    finish tailwind config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 type Props = {};
 
 const App = ({}: Props) => {
-  return <h1 className="text-3xl font-bold"></h1>;
+  return <h1 className="text-4xl font-bold underline">Hello world</h1>;
 };
 
 export default App;
