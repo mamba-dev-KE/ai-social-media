28fcffa027dfb0b764ae6dc841c706023b7f1e72
Joseph Maramba <joseph@smartmile.eu>
2023-01-27 12:44:32 +0300

    setup routing with react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home, CreatePost } from './pages';
+import { Header } from './components';
+
 type Props = {};
 
 const App = ({}: Props) => {
-  return <h1 className="text-4xl font-bold underline">Hello world</h1>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreatePost />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
